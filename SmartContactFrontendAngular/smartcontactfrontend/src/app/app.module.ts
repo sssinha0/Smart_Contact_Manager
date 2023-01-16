1c1840a49b0b7364dd8e79764de984a82d219d20
sssinha0 <b180043@nitsikkim.ac.in>
2023-01-16 10:19:08 +0530

    Angular Forntend

--- SmartContactFrontendAngular/smartcontactfrontend/src/app/app.module.ts
+++ SmartContactFrontendAngular/smartcontactfrontend/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './UserDashboard/login/login.component';
 import { SignupComponent } from './UserDashboard/signup/signup.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,8 +28,19 @@ import { FooterComponent } from './pages/footer/footer.component';
     FooterComponent
   ],
   imports: [
+    MatDividerModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatListModule,
+    MatSelectModule,
+    MatIconModule,
+    MatGridListModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
